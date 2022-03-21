aae1cb8a3ebf5630e8b9aaefc6697de67b2a1b29
Amrsatrio <satrionareswara@yahoo.com>
2022-03-21 12:04:56 +0700

    Update FortPersistentResourceItemDefinition.java

--- src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortPersistentResourceItemDefinition.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortPersistentResourceItemDefinition.java
@@ -1,5 +1,13 @@
 package me.fungames.jfortniteparse.fort.exports;
 
+import me.fungames.jfortniteparse.ue4.objects.core.i18n.FText;
+import me.fungames.jfortniteparse.ue4.objects.uobject.FSoftObjectPath;
+
 public class FortPersistentResourceItemDefinition extends FortAccountItemDefinition {
+    public FSoftObjectPath ItemPreviewActorClass;
     public boolean bIsEventItem = false;
+    public String StatName;
+    public String StatTotalName;
+    public FText ExclusiveDesciption;
+    public FSoftObjectPath ExclusiveIcon;
 }
