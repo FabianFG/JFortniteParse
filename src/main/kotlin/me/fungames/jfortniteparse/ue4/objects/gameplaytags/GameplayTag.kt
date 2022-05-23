8950ebe7ba7e66968408e89d73c902cb697f7432
Amrsatrio <satrionareswara@yahoo.com>
2022-05-23 17:50:12 +0700

    Add hashCode() and equals() to FGameplayTag

--- src/main/kotlin/me/fungames/jfortniteparse/ue4/objects/gameplaytags/GameplayTag.kt
+++ src/main/kotlin/me/fungames/jfortniteparse/ue4/objects/gameplaytags/GameplayTag.kt
@@ -6,4 +6,13 @@ import me.fungames.jfortniteparse.ue4.objects.uobject.FName
 @UStruct
 class FGameplayTag(@JvmField var TagName: FName = FName.NAME_None) {
     override fun toString() = TagName.toString()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is FGameplayTag) return false
+
+        return TagName == other.TagName
+    }
+
+    override fun hashCode() = TagName.toString().toLowerCase().hashCode()
 }
