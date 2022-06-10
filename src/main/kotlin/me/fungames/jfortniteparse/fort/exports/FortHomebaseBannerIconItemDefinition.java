7fc2fdf3a8fb0852988a43f05f7f54a2ee8ee3c5
GewoonIraj <iraj-parki@hotmail.com>
2022-06-10 17:31:58 +0200

    Category --> category (#15)

--- src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortHomebaseBannerIconItemDefinition.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortHomebaseBannerIconItemDefinition.java
@@ -3,6 +3,6 @@ package me.fungames.jfortniteparse.fort.exports;
 import me.fungames.jfortniteparse.ue4.objects.FDataTableRowHandle;
 
 public class FortHomebaseBannerIconItemDefinition extends AthenaCosmeticItemDefinition {
-    public FDataTableRowHandle Category;
+    public FDataTableRowHandle category;
     public Boolean bFullUsageRights;
 }
