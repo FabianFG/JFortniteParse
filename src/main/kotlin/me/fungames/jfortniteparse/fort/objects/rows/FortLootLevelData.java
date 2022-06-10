6fce8cb4df625ec6d36c2f3419ab6a5870b32045
GewoonIraj <iraj-parki@hotmail.com>
2022-06-10 17:30:38 +0200

    Revert "Update FortLootLevelData.java"
    This reverts commit 9a87e7e65763738d6c88fa62642af733b61ba98c.

--- src/main/kotlin/me/fungames/jfortniteparse/fort/objects/rows/FortLootLevelData.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/objects/rows/FortLootLevelData.java
@@ -4,7 +4,7 @@ import me.fungames.jfortniteparse.ue4.objects.FTableRowBase;
 import me.fungames.jfortniteparse.ue4.objects.uobject.FName;
 
 public class FortLootLevelData extends FTableRowBase {
-    public FName category;
+    public FName Category;
     public int LootLevel;
     public int MinItemLevel;
     public int MaxItemLevel;
