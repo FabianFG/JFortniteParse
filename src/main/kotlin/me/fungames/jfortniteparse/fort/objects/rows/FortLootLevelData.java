9a87e7e65763738d6c88fa62642af733b61ba98c
GewoonIraj <iraj-parki@hotmail.com>
2022-06-10 17:24:29 +0200

    Update FortLootLevelData.java

--- src/main/kotlin/me/fungames/jfortniteparse/fort/objects/rows/FortLootLevelData.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/objects/rows/FortLootLevelData.java
@@ -4,7 +4,7 @@ import me.fungames.jfortniteparse.ue4.objects.FTableRowBase;
 import me.fungames.jfortniteparse.ue4.objects.uobject.FName;
 
 public class FortLootLevelData extends FTableRowBase {
-    public FName Category;
+    public FName category;
     public int LootLevel;
     public int MinItemLevel;
     public int MaxItemLevel;
