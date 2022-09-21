4d846d674662b95d22df9b345340896674f626e5
Amrsatrio <satrionareswara@yahoo.com>
2022-09-22 05:54:08 +0700

    Add VariantChannelTag for FortCosmeticLoadoutTagDrivenVariant

--- src/main/kotlin/me/fungames/jfortniteparse/fort/exports/variants/FortCosmeticLoadoutTagDrivenVariant.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/exports/variants/FortCosmeticLoadoutTagDrivenVariant.java
@@ -2,13 +2,19 @@ package me.fungames.jfortniteparse.fort.exports.variants;
 
 import me.fungames.jfortniteparse.fort.objects.variants.PartVariantDef;
 import me.fungames.jfortniteparse.ue4.assets.UStruct;
+import me.fungames.jfortniteparse.ue4.objects.gameplaytags.FGameplayTag;
 import me.fungames.jfortniteparse.ue4.objects.gameplaytags.FGameplayTagContainer;
+import me.fungames.jfortniteparse.ue4.objects.uobject.FName;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class FortCosmeticLoadoutTagDrivenVariant extends FortCosmeticVariantBackedByArray {
     public List<TagDrivenVariantDef> Variants;
+
+    public FortCosmeticLoadoutTagDrivenVariant() {
+        VariantChannelTag = new FGameplayTag(new FName("Cosmetics.Variant.Channel.TagDriven")); // TODO check later
+    }
 
     @Nullable
     @Override
