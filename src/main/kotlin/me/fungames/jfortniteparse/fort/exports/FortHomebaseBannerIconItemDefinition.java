9879a18337a254cc03a45b75e664a69987a30851
Amrsatrio <satrionareswara@yahoo.com>
2023-05-04 10:40:59 +0700

    Fix for Fortnite 24.30

--- src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortHomebaseBannerIconItemDefinition.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/exports/FortHomebaseBannerIconItemDefinition.java
@@ -3,6 +3,6 @@ package me.fungames.jfortniteparse.fort.exports;
 import me.fungames.jfortniteparse.ue4.objects.FDataTableRowHandle;
 
 public class FortHomebaseBannerIconItemDefinition extends AthenaCosmeticItemDefinition {
-    public FDataTableRowHandle category;
+    public FDataTableRowHandle Category;
     public Boolean bFullUsageRights;
 }
